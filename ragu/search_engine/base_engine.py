3385399276463ae3e9c3d89920416cdbeddc93ab
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-10-08 03:20:02 +0700

    Refactor base class for search engines

--- ragu/search_engine/base_engine.py
+++ ragu/search_engine/base_engine.py
@@ -1,27 +1,43 @@
-from abc import abstractmethod, ABC
+from abc import ABC, abstractmethod
+
+from pydantic import BaseModel
+
+from ragu.common.base import RaguGenerativeModule
+from ragu.utils.ragu_utils import always_get_an_event_loop
 
 
-class BaseEngine(ABC):
+class BaseEngine(RaguGenerativeModule, ABC):
     def __init__(self, *args, **kwargs):
-        ...
+        super().__init__(*args, **kwargs)
 
     @abstractmethod
-    def build_index(self):
-        """
-        Build index for knowledge graph
-        """
-        pass
-
-    @abstractmethod
-    def search(self, query, *args, **kwargs):
+    async def a_search(self, query, *args, **kwargs):
         """
         Get relevant information from knowledge graph
         """
         pass
 
     @abstractmethod
-    def query(self, query: str):
+    async def a_query(self, query: str) -> BaseModel:
         """
         Get answer on query from knowledge graph
         """
         pass
+
+    async def query(self, query: str) -> BaseModel:
+        """
+        Get answer on query from knowledge graph
+        """
+        loop = always_get_an_event_loop()
+        return loop.run_until_complete(
+            self.a_query(query)
+        )
+
+    async def search(self, query, *args, **kwargs):
+        """
+        Get relevant information from knowledge graph
+        """
+        loop = always_get_an_event_loop()
+        return loop.run_until_complete(
+            self.a_search(query, *args, **kwargs)
+        )
