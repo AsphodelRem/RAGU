a66bb1b62dad7b1cacc3086f01e12e0fb316e88c
AsphodelRem <asphodel.bog.rem@gmail.com>
2025-10-08 03:46:05 +0700

    Update global parameters: add new name and global settings

--- ragu/common/global_parameters.py
+++ ragu/common/global_parameters.py
@@ -1,31 +1,47 @@
-import logging
 import os
 from datetime import datetime
 
+from loguru import logger
+
+
 DEFAULT_FILENAMES = {
-    "community_summary_kv_storage_name": "community_summary_kv.json",
-    "chunks_kv_storage_name": "chunks_kv.json",
-    "entity_vdb_name": "entity_vdb.json",
-    "relation_vdb_name": "relation_vdb.json",
-    "chunks_vdb_name": "chunks_vdb.json",
-    "graph_name": "graph.gml",
+    "community_kv_storage_name": "kv_community.json",
+    "chunks_kv_storage_name": "kv_chunks.json",
+    "entity_vdb_name": "vdb_entity.json",
+    "relation_vdb_name": "vdb_relation.json",
+    "knowledge_graph_storage_name": "knowledge_graph.gml",
+    "community_summary_kv_storage_name": "kv_community_summary.json",
 }
 
-current_dir = os.getcwd()
-working_dir = os.path.join(current_dir, "ragu_working_dir")
-current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
 
-# Folder for current run
-current_run = os.path.join(working_dir, current_time)
-os.makedirs(current_run, exist_ok=True)
+class GlobalSettings:
+    __instance = None
+    _current_time = datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+    _working_dir = os.path.join(os.path.join(os.getcwd(), "ragu_working_dir"), _current_time)
 
-# Folders that store knowledge graph outputs and indexer data
-logs_dir = os.path.join(current_run, "logs")
-run_output_dir = os.path.join(current_run, "outputs")
-storage_run_dir = os.path.join(current_run, "storage")
+    def __new__(cls, *args, **kwargs):
+        if cls.__instance is None:
+            cls.__instance = super(GlobalSettings, cls).__new__(cls)
+            return cls.__instance
+        else:
+            return cls.__instance
 
-os.makedirs(logs_dir, exist_ok=True)
-os.makedirs(run_output_dir, exist_ok=True)
-os.makedirs(storage_run_dir, exist_ok=True)
+    @property
+    def storage_folder(self):
+        return self._working_dir
+
+    @storage_folder.setter
+    def storage_folder(self, path):
+        self._working_dir = path
+
+    def init_storage_folder(self):
+        if not os.path.exists(self._working_dir):
+            logger.info(f"Creating folder for current run: {self._working_dir}")
+            os.makedirs(self._working_dir, exist_ok=True)
+
+
+Settings = GlobalSettings()
+
+
 
 
